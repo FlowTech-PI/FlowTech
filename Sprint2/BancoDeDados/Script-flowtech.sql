fcc558427f2c2820b87aaaa8fd64088b642b7228
m-de-mello <murillo.mello@sptech.school>
2024-04-16 15:30:10 -0300

    Atualização Script - Banco de Dados v1.1

--- Sprint2/BancoDeDados/Script-flowtech.sql
+++ Sprint2/BancoDeDados/Script-flowtech.sql
@@ -1,113 +1,119 @@
-create database flowtech;
-use flowtech;
+CREATE DATABASE flowtech;
+USE flowtech;
 
 -- Criação das tabelas 
 
-create table empresa
+CREATE TABLE empresa
 (
-idEmpresa int primary key auto_increment,
-nome varchar(45),
-cnpj char(14),
-cep char(8),
-telefone char(11),
-email varchar(45)
+idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(45),
+cnpj CHAR(14),
+cep CHAR(8),
+telefone CHAR(11),
+email VARCHAR(45)
 );
 
-create table funcionario
+CREATE TABLE funcionario
 (
-idUsuario int primary key auto_increment,
-nome varchar(100),
-dtNasc date,
-cpf char(11),
-telefone char(11),
-email varchar(45),
-senha varchar(20),
-funcao varchar(45),
-fkAdmin int,
-fkEmpresa int,
-constraint fkAdminFunc foreign key (fkAdmin)
-references funcionario (idUsuario),
-constraint fkEmpresaFunc foreign key (fkEmpresa)
-references empresa (idEmpresa) 
+idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(100),
+dtNasc DATE,
+cpf CHAR(11),
+telefone CHAR(11),
+email VARCHAR(45),
+senha VARCHAR(20),
+funcao VARCHAR(45),
+fkAdmin INT,
+fkEmpresa INT,
+CONSTRAINT fkAdminFunc FOREIGN KEY (fkAdmin)
+	REFERENCES funcionario(idUsuario),
+CONSTRAINT fkEmpresaFunc FOREIGN KEY (fkEmpresa)
+	REFERENCES empresa(idEmpresa),
+CONSTRAINT chkFuncao CHECK (funcao IN('Administrador', 'Gerente', 'Funcionário'))
 );
-alter table funcionario add constraint chkfuncao check (funcao in('Administrador', 'Gerente', 'Funcionário'));
 
-create table linha 
+CREATE TABLE linha 
 (
-idLinha int primary key,
-nome varchar(45),
-fkEmpresa int,
-constraint fkEmpresaLinha foreign key (fkEmpresa)
-references empresa (idEmpresa)
+idLinha INT PRIMARY KEY,
+nome VARCHAR(45),
+fkEmpresa INT,
+CONSTRAINT fkEmpresaLinha FOREIGN KEY (fkEmpresa)
+	REFERENCES empresa(idEmpresa)
 );
 
 create table estacao
 (
-idEstacao int primary key auto_increment,
-nome varchar(45),
-fkLinha int,
-constraint fkLinhaEstacao foreign key (fklinha)
-references linha (idLinha)
+idEstacao INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(45),
+fkLinha INT,
+CONSTRAINT fkLinhaEstacao FOREIGN KEY (fklinha)
+	REFERENCES linha(idLinha)
 );
 
 create table sensor
 (
-idSensor int primary key auto_increment,
-nomeLocal varchar(30),
-tipoSensor varchar(40),
-fkEstacao int,
-constraint fkEstacaoSensor foreign key (fkEstacao)
-references estacao(idEstacao)
+idSensor INT PRIMARY KEY AUTO_INCREMENT,
+nomeLocal VARCHAR(30),
+tipoSensor VARCHAR(40),
+fkEstacao INT,
+CONSTRAINT fkEstacaoSensor FOREIGN KEY (fkEstacao)
+	REFERENCES estacao(idEstacao)
 );
 
-create table dadoSensor
+CREATE TABLE dadoSensor
 (
-idDado int primary key auto_increment,
-contagem bit,
-horario datetime default current_timestamp,
-fkSensor int,
-constraint fkSensorDadoSensor foreign key (fkSensor)
-references sensor(idSensor)
+idDado INT PRIMARY KEY AUTO_INCREMENT,
+contagem BIT,
+horario DATETIME DEFAULT CURRENT_TIMESTAMP,
+fkSensor INT,
+CONSTRAINT fkSensorDadoSensor FOREIGN KEY (fkSensor)
+	REFERENCES sensor(idSensor)
 );
 
 -- Inserção de Dados 
 
-insert into empresa values
-(default, 'ViaQuatro', '07682638000107','09340520', '08007707100', 'comercial.mobilidade@grupoccr.com');
-select * from empresa;
-insert into funcionario values
-(default,  'Josival da Silva Santos', '1972-01-01', '25448868820', '11986457457', 'josival.santos@gmail.com', 'josival0101', 'Administrador', null, 1),
-(default,  'Alberto Vicente Alves', '1980-04-22', '29033534843', '11985451260', 'alberto.alves@gmail.com', 'alberto@123', 'Gerente', 1, 1),
-(default,  'Rafael Henrique Silva', '1990-02-20', '53589878841', '11959382646', 'rafael.silva@gmail.com', 'rafael227755', 'Funcionário',2, 1);
-select * from funcionario;
+INSERT INTO empresa VALUES
+	(DEFAULT, 'ViaQuatro', '07682638000107','09340520', '08007707100', 'comercial.mobilidade@grupoccr.com');
 
-insert into linha values 
-(4, 'Amarela', 1);
-select * from linha;
+SELECT * FROM empresa;
 
-insert into estacao values
-(default, 'Vila Sônia', 4),
-(default, 'São Paulo - Morumbi', 4),
-(default, 'Butantã', 4),
-(default, 'Pinheiros', 4),
-(default, 'Faria Lima', 4),
-(default, 'Fradique Coutinho', 4),
-(default, 'Oscar Freire', 4),
-(default, 'Paulista - Pernambucanas', 4),
-(default, 'Higienópolis - Mackenzie', 4),
-(default, 'República', 4),
-(default, 'Luz', 4);
-select * from estacao;
+INSERT INTO funcionario VALUES
+	(DEFAULT,  'Josival da Silva Santos', '1972-01-01', '25448868820', '11986457457', 'josival.santos@gmail.com', 'josival0101', 'Administrador', null, 1),
+	(DEFAULT,  'Alberto Vicente Alves', '1980-04-22', '29033534843', '11985451260', 'alberto.alves@gmail.com', 'alberto@123', 'Gerente', 1, 1),
+	(DEFAULT,  'Rafael Henrique Silva', '1990-02-20', '53589878841', '11959382646', 'rafael.silva@gmail.com', 'rafael227755', 'Funcionário',2, 1);
+SELECT * FROM funcionario;
 
-insert into sensor values
-(default, 'esacada rolante 1', 'Sensor de bloqueio', 8),
-(default, 'Passarela', 'Sensor de bloqueio', 11),
-(default, 'Catraca de entrada', 'Sensor de bloqueio', 2),
-(default, 'esacada rolante 2', 'Sensor de bloqueio', 1);
-select * from sensor;
+INSERT INTO linha VALUES
+	(4, 'Amarela', 1);
 
-insert into dadoSensor values
-(default, 1, default, 2),
-(default, 0, '2024-04-14 06:00:00', 3),
-(default, 1, default, 4);
-select * from dadoSensor;
+SELECT * FROM linha;
+
+INSERT INTO estacao VALUES
+	(DEFAULT, 'Vila Sônia', 4),
+	(DEFAULT, 'São Paulo - Morumbi', 4),
+	(DEFAULT, 'Butantã', 4),
+	(DEFAULT, 'Pinheiros', 4),
+	(DEFAULT, 'Faria Lima', 4),
+	(DEFAULT, 'Fradique Coutinho', 4),
+	(DEFAULT, 'Oscar Freire', 4),
+	(DEFAULT, 'Paulista - Pernambucanas', 4),
+	(DEFAULT, 'Higienópolis - Mackenzie', 4),
+	(DEFAULT, 'República', 4),
+	(DEFAULT, 'Luz', 4);
+
+SELECT * FROM estacao;
+
+INSERT INTO sensor VALUES
+	(DEFAULT, 'esacada rolante 1', 'Sensor de bloqueio', 8),
+	(DEFAULT, 'Passarela', 'Sensor de bloqueio', 11),
+	(DEFAULT, 'Catraca de entrada', 'Sensor de bloqueio', 2),
+	(DEFAULT, 'esacada rolante 2', 'Sensor de bloqueio', 1);
+
+SELECT * FROM sensor;
+
+INSERT INTO dadoSensor VALUES
+	(DEFAULT, 1, DEFAULT, 2),
+	(DEFAULT, 0, '2024-04-14 06:00:00', 3),
+	(DEFAULT, 1, DEFAULT, 4);
+
+SELECT * FROM dadoSensor;
